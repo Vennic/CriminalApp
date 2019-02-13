05ff085ac7bf3636b6479ccef4793ccb8c7dca9c
Adel Kuzheev <kuzheevadel@gmail.com>
2019-02-13 23:09:21 +0500

    Add Dialog(DatePicker)

--- app/src/main/java/com/example/crimeapp/CrimeFragment.java
+++ app/src/main/java/com/example/crimeapp/CrimeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -27,6 +28,7 @@ public class CrimeFragment extends Fragment {
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
     private static final String CRIME_ID = "android_crime_fragment_ID";
+    private static final String DIALOG_ID = "android_crime_dialogFragment_ID";
 
     public static CrimeFragment newInstance(UUID id) {
         Bundle args = new Bundle();
@@ -52,7 +54,14 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment datePickerFragment = new DatePickerFragment();
+                datePickerFragment.show(manager, DIALOG_ID);
+            }
+        });
 
         mSolvedCheckBox = v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
