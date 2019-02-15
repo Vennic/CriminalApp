2fb653dcea922cdc926528c0867c0790962c4bc3
Adel Kuzheev <kuzheevadel@gmail.com>
2019-02-15 23:06:00 +0500

    Add "addButton"

--- app/src/main/java/com/example/crimeapp/CrimeListActivity.java
+++ app/src/main/java/com/example/crimeapp/CrimeListActivity.java
@@ -1,10 +1,56 @@
 package com.example.crimeapp;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class CrimeListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends AppCompatActivity {
+
+    Button addButton;
+
     @Override
-    protected Fragment createFragment() {
-        return new CrimeListFragment();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        addButton = findViewById(R.id.add_button);
+        if (CrimeLab.get(this).getCrimes().size() == 0) {
+            addButton.setVisibility(View.VISIBLE);
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Crime crime = new Crime();
+                    CrimeLab.get(CrimeListActivity.this).getCrimes().add(crime);
+                    Intent intent = CrimePagerActivity.newIntent(CrimeListActivity.this, crime.getId());
+                    startActivity(intent);
+                }
+            });
+        } else {
+            addButton.setVisibility(View.INVISIBLE);
+        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeListFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (CrimeLab.get(this).getCrimes().size() == 0) {
+            addButton.setVisibility(View.VISIBLE);
+        } else {
+            addButton.setVisibility(View.INVISIBLE);
+        }
     }
 }
